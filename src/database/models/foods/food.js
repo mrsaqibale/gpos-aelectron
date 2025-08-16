e7d2e6e199cf5113854cb38ff21059f625c5f130
mrsaqibale <kalikali72547254@gmail.com>
2025-08-16 11:38:31 +0500

    fix bug food get by id

--- src/database/models/foods/food.js
+++ src/database/models/foods/food.js
@@ -566,7 +566,7 @@ export function searchFoodsByName(name, restaurant_id = 1) {
   } catch (err) {
     return errorResponse(err.message);
   }
-}
+} 
 
 // Get food ingredients
 export function getFoodIngredients(foodId) {
