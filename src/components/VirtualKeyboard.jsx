954f4340d20f728d901001cbf8d7d2aab752dff7
Ayeza Bashir <ayezabashir46@gmail.com>
2025-08-18 19:07:08 +0500

    fixed the capslock bug

--- src/components/VirtualKeyboard.jsx
+++ src/components/VirtualKeyboard.jsx
@@ -194,14 +194,14 @@ const VirtualKeyboard = ({
               default: [
                 "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
                 "{tab} q w e r t y u i o p [ ] \\",
-                "{caps} a s d f g h j k l ; ' {enter}",
+                "{lock} a s d f g h j k l ; ' {enter}",
                 "z x c v b n m , . /",
                 "{space}"
               ],
               shift: [
                 "~ ! @ # $ % ^ & * ( ) _ + {bksp}",
                 "{tab} Q W E R T Y U I O P { } |",
-                "{caps} A S D F G H J K L : \" {enter}",
+                "{lock} A S D F G H J K L : \" {enter}",
                 "Z X C V B N M < > ?",
                 "{space}"
               ]
