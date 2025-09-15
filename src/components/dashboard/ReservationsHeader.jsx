93c393a3cd56a9e0f88591d8d98abcffa23414b1
mrsaqibale <kalikali72547254@gmail.com>
2025-09-16 00:14:13 +0500

    udpate reservatinos

--- src/components/dashboard/ReservationsHeader.jsx
+++ src/components/dashboard/ReservationsHeader.jsx
@@ -8,11 +8,9 @@ const ReservationsHeader = () => {
   return (
     <div>
       <div className="flex gap-2 items-center p-2">
-        <button className="py-4 px-4 rounded-md bg-primary text-white text-sm font-medium cursor-pointer">All Reservations</button>
         <button className="py-4 px-4 rounded-md bg-primary text-white text-sm font-medium cursor-pointer flex items-center gap-1" onClick={() => setShowModal(true)}>
            New Reservation
         </button>
-        <button className="py-4 px-4 rounded-md bg-primary text-white text-sm font-medium cursor-pointer">Today’s Reservations</button>
         <button className="py-4 px-4 rounded-md bg-primary text-white text-sm font-medium cursor-pointer" onClick={() => navigate('/dashboard/sales')}>Sales Screen</button>
       </div>
       <NewReservation isOpen={showModal} onClose={() => setShowModal(false)} onCreate={() => setShowModal(false)} />
