822a529e47bcaa8347f6d57e8123f6c1d58fd5ae
mrsaqibale <kalikali72547254@gmail.com>
2025-09-16 22:15:28 +0500

    fix side bar remove corners

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -116,7 +116,7 @@ const Sidebar = ({ navigationItems }) => {
       <div
         className={`lg:hidden fixed inset-y-0 left-0 z-40 w-64 transform transition-transform duration-300 ease-in-out ${
           isMobileMenuOpen ? "translate-x-0" : "-translate-x-full"
-        } shadow-xl rounded-r-xl`}
+        } shadow-xl`}
         style={{ backgroundColor: themeColors.primary }}
       >
         <div 
