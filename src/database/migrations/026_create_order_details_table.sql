57422105b187d4611c61344c140ce30e8ad28603
mrsaqibale <kalikali72547254@gmail.com>
2025-09-19 22:08:33 +0500

    update order details table

--- src/database/migrations/026_create_order_details_table.sql
+++ src/database/migrations/026_create_order_details_table.sql
@@ -17,6 +17,8 @@ CREATE TABLE IF NOT EXISTS order_details (
     total_add_on_price REAL DEFAULT 0,
     issynicronized BOOLEAN DEFAULT 0,
     isdeleted BOOLEAN DEFAULT 0,
+    iscreateyourown BOOLEAN DEFAULT 0,
+    isopen BOOLEAN DEFAULT 0,
     FOREIGN KEY (food_id) REFERENCES food(id),
     FOREIGN KEY (order_id) REFERENCES orders(id)
 ); 
